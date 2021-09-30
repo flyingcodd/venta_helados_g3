367913ede45aa69fe127658f8c918320ecb3d558
trixus0510 <jaimecarrascotaipe@gmail.com>
2021-09-29 22:46:05 -0500

    categorias

--- app/Views/webCliente/shop/tienda.php
+++ app/Views/webCliente/shop/tienda.php
@@ -25,51 +25,51 @@
                     <div class="shop-sidebar">
                         <!-- Single Widget -->
                         <div class="single-widget category">
-                            <h3 class="title">Categories</h3>
+                            <h3 class="title">Categorias</h3>
                             <ul class="categor-list">
-                                <li><a href="#">T-shirts</a></li>
-                                <li><a href="#">jacket</a></li>
-                                <li><a href="#">jeans</a></li>
-                                <li><a href="#">sweatshirts</a></li>
-                                <li><a href="#">trousers</a></li>
-                                <li><a href="#">kitwears</a></li>
-                                <li><a href="#">accessories</a></li>
+                                <li><a href="#">Helado de Crema</a></li>
+                                <li><a href="#">Helado de Leche</a></li>
+                                <li><a href="#">Helado de Fruta</a></li>
+                                <li><a href="#">Raspadillas</a></li>
+                                <li><a href="#">Helados en Cono</a></li>
+                                <li><a href="#">Marcianos</a></li>
+                                <li><a href="#">Helado de Espuma</a></li>
                             </ul>
                         </div>
                         <!--/ End Single Widget -->
                         <!-- Shop By Price -->
                         <div class="single-widget range">
-                            <h3 class="title">Shop by Price</h3>
+                            <h3 class="title">Filtrar por precio</h3>
                             <div class="price-filter">
                                 <div class="price-filter-inner">
                                     <div id="slider-range"></div>
                                     <div class="price_slider_amount">
                                         <div class="label-input">
-                                            <span>Range:</span><input type="text" id="amount" name="price" placeholder="Add Your Price" />
+                                            <span>Rango:</span><input type="text" id="amount" name="price" placeholder="Add Your Price" />
                                         </div>
                                     </div>
                                 </div>
                             </div>
                             <ul class="check-box-list">
                                 <li>
-                                    <label class="checkbox-inline" for="1"><input name="news" id="1" type="checkbox">$20 - $50<span class="count">(3)</span></label>
+                                    <label class="checkbox-inline" for="1"><input name="news" id="1" type="checkbox">S/.10 - S/.30<span class="count">(3)</span></label>
                                 </li>
                                 <li>
-                                    <label class="checkbox-inline" for="2"><input name="news" id="2" type="checkbox">$50 - $100<span class="count">(5)</span></label>
+                                    <label class="checkbox-inline" for="2"><input name="news" id="2" type="checkbox">S/.30 - S/.60<span class="count">(5)</span></label>
                                 </li>
                                 <li>
-                                    <label class="checkbox-inline" for="3"><input name="news" id="3" type="checkbox">$100 - $250<span class="count">(8)</span></label>
+                                    <label class="checkbox-inline" for="3"><input name="news" id="3" type="checkbox">S/.60 - S/100<span class="count">(8)</span></label>
                                 </li>
                             </ul>
                         </div>
                         <!--/ End Shop By Price -->
                         <!-- Single Widget -->
                         <div class="single-widget recent-post">
-                            <h3 class="title">Recent post</h3>
+                            <h3 class="title">Novedades</h3>
                             <!-- Single Post -->
                             <div class="single-post first">
                                 <div class="image">
-                                    <img src="https://via.placeholder.com/75x75" alt="#">
+                                    <img src="https://www.hola.com/imagenes/cocina/noticiaslibros/20210629192230/recetas-helados-cucurucho-cornete/0-969-441/cornetes-helados-adob-m.jpg" alt="#">
                                 </div>
                                 <div class="content">
                                     <h5><a href="#">Girls Dress</a></h5>
